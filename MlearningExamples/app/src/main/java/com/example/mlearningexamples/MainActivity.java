bb0719b73a76e20af245e2ea7d1f069c5f8df329
geet thakur <thakurgeet8@gmail.com>
2019-02-14 17:52:47 +0530

    avadesh Home Activity created.

--- MlearningExamples/app/src/main/java/com/example/mlearningexamples/MainActivity.java
+++ MlearningExamples/app/src/main/java/com/example/mlearningexamples/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.mlearningexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private final String[] list_items = {"Arun", "Akansha", "Avadhesh", "Geet"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView user_list = findViewById(R.id.user_list);
+        RecyclerView.LayoutManager lm= new LinearLayoutManager(this);
+        user_list.setLayoutManager(lm);
 
+
+
+        // specify an adapter (see also next example)
+        MyAdapter  mAdapter = new MyAdapter(list_items);
+        user_list.setAdapter(mAdapter);
 
 
     }
---
+++ MlearningExamples/app/src/main/java/com/example/mlearningexamples/avadesh/AvadeshActivity.java
@@ -0,0 +1,15 @@
+package com.example.mlearningexamples.avadesh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mlearningexamples.R;
+
+public class AvadeshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avadesh);
+    }
+}
